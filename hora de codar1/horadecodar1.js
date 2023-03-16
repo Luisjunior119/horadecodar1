2fdbcbd7d09df6a08633b2040ed9479a711f587f
LUIS DE SANTANA TAVARES JUNIOR <luis.stjunior@senacsp.edu.br>
2023-03-16 17:27:04 -0300

    first commit

---
+++ hora de codar1/horadecodar1.js
@@ -0,0 +1,83 @@
+
+/*#1
+var modelo_do_carro = 'Fusquinha'
+alert(modelo_do_carro)*/
+
+/*#2
+Escreva um programa em Javascript em que o usuário informe o seu
+nome e exiba a mensagem "Olá, [NomeDoUsuario]".
+
+var nome = prompt('digite seu nome')
+alert ('Olá, ' + nome)*/
+
+/*#3
+Escreva um programa em Javascript em que o usuário informe o seu
+nome e em seguida o programa perguntará a idade do usuário. Agora o 
+programa deve exibir a mensagem "Olá, [NomeDoUsuario], sua idade é [idade]".
+
+var nome = prompt('Digite seu nome aqui, meu consagra!')
+var idade = parseInt(prompt('E quantos anos você tem?'))
+alert('Olá, ' + nome + ', você tem ' + idade + ' anos de idade.')*/
+
+/*#4 retângulo
+Considerando a figura acima, escreva um programa para cada forma que calcule e exiba em
+ tela cada uma de suas respectivas áreas. O usuário irá informar os valores de cada variável.
+
+    var base = (parseInt(prompt('Digite aqui o valor da base')))
+    var altura = (parseInt(prompt('Digite aqui o valor da altura')))
+
+    var area = (base * altura)
+    alert(`A área é ${area}`)*/
+
+/*#5 quadrado
+    var lado1 = parseInt(prompt('Digite o valor de um dos lados do quadrado'))
+    var lado2 = parseInt(prompt('Digite o valor do lado do quadrado que falta'))
+    var area = lado1 * lado2
+    alert('a área do quadrado é:' + area)*/
+/*#6
+    var loma = prompt('Digite o valor da diagonal maior aqui');
+    var lome = parseInt(prompt('Digite aqui o valor diagonal menor '));
+    var area = loma * lome / 2;
+
+    alert('A área total do losango é: ' + area)*/
+
+/* var basema = Number(prompt('Digite o valor da base maior'));
+    var baseme = Number(prompt('Digite o valor da base menor'));
+    var altura = Number(prompt('Digite o valor da altura'));
+
+    var area = (basema + baseme) * altura / 2;
+    alert("A área do trapézio é: " + area)*/
+
+/*#8
+    var base = prompt('Digite o valor da base')
+    var altura = parseInt(prompt('Digite o valor da altura'))
+    var area = base * altura
+    alert('A área do paralelogramo é :' + area)*/
+/*#9
+    var base = prompt('Digite o valor da base do triângulo')
+    var altura = parseInt(prompt('Digite o valor da altura do triângulo'))
+    var area = base * altura / 2
+    alert('A área do triângulo é: ' + area)*/
+
+/*#10
+   var raio = parseInt(prompt('Digite o valor do raio'))
+   var area = (3,14 * (raio**2))
+    alert('a área do circulo é: ' + area)*/
+
+
+//Para exibir uma mensagem de texto ao usuário.
+
+//Para o usuário colocar informações e logo em seguida ele retorna com as informações que foram inseridas anteriormente, mas em forma de string.
+
+//Para exibir o valor das variáveis no console.
+
+
+
+   
+
+   
+
+
+
+    
+  
